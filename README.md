375496b9ee873490d66161c5eced445fe3a3c0a5
12944qwerty <qwerty12944qwerty@gmail.com>
2023-08-02 00:40:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Better Codeblocks
+
+Improves the codeblocks. Addes line numbers and a copy button.
+
+### Todo
+Implement Shiki
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Injector, Logger, common, components } from "replugged";
+import Codeblock from "./Codeblock";
+
+const { parser, React } = common;
+const { ErrorBoundary } = components;
+
+import './index.css';
+
+const injector = new Injector();
+
+export function start(): void {
+  injector.after(parser.defaultRules.codeBlock, "react", (args, _) => {
+    const { lang, content: code } = args[0] as { lang: string, content: string };
+
+    return React.createElement(ErrorBoundary, {}, React.createElement(Codeblock, {
+      lang,
+      code,
+    }));
+  });
+}
+
+export function stop(): void {
+  injector.uninjectAll();
+}
